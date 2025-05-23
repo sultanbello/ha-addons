8472aa1d7803329209d42750e40d8336b96ab33e
Tsjippy <ewaldha@hotmail.com>
2025-05-23 12:25:31 +0100

    fix

--- signal-reply/run.sh
+++ signal-reply/run.sh
@@ -6,15 +6,15 @@ source /data/venv/bin/activate
 
 bashio::log.info "Checking prerequisites"
 
-if [ ! -f /data/pip ]; then
+#if [ ! -f /data/pip ]; then
     bashio::log.info "Installing prerequisites"
 
     python3 -m ensurepip --upgrade >/dev/null
 
-    pip install --upgrade websocket-client rel requests >/dev/null
+    pip install --upgrade websocket-client rel requests google-api-python-client google-auth-httplib2 google-auth-oauthlib lxml >/dev/null
 
     echo "pip done" > /data/pip
-fi
+#fi
 
 bashio::log.info "Starting Script"
 
