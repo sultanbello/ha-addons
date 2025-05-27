8a98ed4bc141cc67999db4188789200677c95b59
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2025-05-27 12:48:02 +0100

    Update google_contacts.py

--- signal-reply/scripts/google_contacts.py
+++ signal-reply/scripts/google_contacts.py
@@ -266,7 +266,7 @@ class Contacts:
                 return 'en'
             
             # we should only come here if we do not have a message in the languague needed
-            self.parent.logger.warning(f"Invalid country {country_code} for user {data.get('name')} [link]({self.link(data.get('url', ''))}). Defaulting to English languague")
+            self.parent.logger.warning(f"Invalid country {country_code} for user {data.get('name')} ({self.link(data.get('url', ''))}). Defaulting to English languague")
             return 'en'
         except Exception as e:
             self.parent.logger.error(f"{str(e)} on line {sys.exc_info()[-1].tb_lineno}")
