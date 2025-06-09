d2c13688bdcd90552c13380bfe83daff228af911
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2025-06-09 12:13:29 +0100

    Update run.sh

--- whatsapp/run.sh
+++ whatsapp/run.sh
@@ -2,21 +2,6 @@
 
 bashio::log.info "Refreshing data"
 
-#if [ ! -f /data/pip ]; then
-    bashio::log.info "Installing curl"
-    apk add --update curl gnupg git 
-
-    bashio::log.info "Installing chromium"
-    apk add --update chromium ffmpeg git
-
-    bashio::log.info "Installing nodejs"
-    apk add --update nodejs npm
-
-    npm i whatsapp-web.js
-
-    echo "pip done" > /data/pip
-#fi
-
 cd /app || exit 1
 exec node dist/src/index.js --port="$(bashio::config 'webserver_port')"
 
