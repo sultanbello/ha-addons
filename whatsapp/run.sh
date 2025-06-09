fea144162faa215aa18596c083433b33a3460af8
Tsjippy <ewaldha@hotmail.com>
2025-06-09 06:29:59 +0100

    update

--- whatsapp/run.sh
+++ whatsapp/run.sh
@@ -2,7 +2,7 @@
 
 bashio::log.info "Refreshing data"
 
-if [ ! -f /data/pip ]; then
+#if [ ! -f /data/pip ]; then
     bashio::log.info "Installing curl"
     apk add --update curl gnupg git 
 
@@ -13,17 +13,19 @@ if [ ! -f /data/pip ]; then
     apk add --update nodejs npm
 
     bashio::log.info "Fetching rest api"
-    git clone --branch "v2.4.1" --depth=1 "https://github.com/gajosu/ha-whatsapp-web-rest-api.git" /app
+    git clone --branch "v2.4.1" --depth=1 "https://github.com/gajosu/ha-whatsapp-web-rest-api.git" /data/app
 
     # install production dependencies
     bashio::log.info "Installing rest api"
     npm install
 
+    npm audit fix --force 
+
     npm update
 
     npm run build
     echo "pip done" > /data/pip
-fi
+#fi
 
 cd /app || exit 1
 exec node dist/src/index.js --port="$(bashio::config 'webserver_port')"
